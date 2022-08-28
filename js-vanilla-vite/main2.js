d36e84d2ae7c8b7431cf2eda889c1443837599de
MrBurp <andreass.ziegler@gmail.com>
2022-08-28 22:45:07 +0200

    import e export

---
+++ js-vanilla-vite/main2.js
@@ -0,0 +1,12 @@
+// import * as math from "./lib/math";
+// console.log(math.sum(2, 3));
+
+//-----------------------------------------------
+
+// import { sum, sub, PI } from "./lib/math";
+
+// console.log(sum(1, 2));
+// console.log(sub(5, 2));
+// console.log(PI);
+
+// or
